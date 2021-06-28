a847d55ee0b8d5fdfe516f33e54f2ead470a8b71
Nathan-MKW <84430521+Nathan-MKW@users.noreply.github.com>
2021-06-27 20:44:35 -0400

    Add files via upload

---
+++ Readings/Reading.js
@@ -0,0 +1,9 @@
+var lightBlue = "#86D5FF";
+var navy = "#1C2D74";
+var mediumBlue = "#2A4ACE";
+
+var colorPalette = [lightBlue,navy,mediumBlue];
+
+//Background
+
+$("body").css("background-color", colorPalette[2]);
