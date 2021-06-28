8ff273ff99a90b4558439da7f3337d96977214f6
Nathan-MKW <84430521+Nathan-MKW@users.noreply.github.com>
2021-06-27 20:44:22 -0400

    Delete Readings directory

--- Readings/Reading.js
+++
@@ -1,9 +0,0 @@
-var lightBlue = "#86D5FF";
-var navy = "#1C2D74";
-var mediumBlue = "#2A4ACE";
-
-var colorPalette = [lightBlue,navy,mediumBlue];
-
-//Background
-
-$("body").css("background-color", colorPalette[2]);
